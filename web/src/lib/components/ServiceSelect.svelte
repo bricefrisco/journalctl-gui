72f444227705d79b289043fa3f01cee268629eba
Brice <bricemfrisco@gmail.com>
2026-01-09 09:45:18 -0600

    add svelte web app

---
+++ web/src/lib/components/ServiceSelect.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let services = [];
+	let selected = '';
+
+	onMount(async () => {
+		const res = await fetch('http://192.168.1.160:8091/api/services');
+		if (!res.ok) throw new Error('Failed to load services');
+		services = await res.json();
+	})
+</script>
+
+<select bind:value={selected}>
+	<option value="" disabled>Select a service</option>
+	{#each services as service (service)}
+		<option value={service}>{service}</option>
+	{/each}
+</select>
